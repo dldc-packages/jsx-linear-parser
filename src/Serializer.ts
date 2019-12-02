39b70bc7fa8e031d256dd1865ddbc5a6e5024c1f
Etienne Dldc <e.deladonchamps@gmail.com>
2019-12-02 23:59:50 +0100

    Add Serializer

---
+++ src/Serializer.ts
@@ -0,0 +1,24 @@
+import { Node, NodeType } from './Node';
+import { Serializer as LiteralSerializer } from 'literal-parser';
+
+export const Serializer = {
+  serialize,
+};
+
+const SERIALIZER: { [K in NodeType]: (node: Node<K>) => string } = {
+  Text: node => node.value,
+  ClosingTag: node => `</${node.component} ${node.props.map(prop => serialize(prop)).join(' ')}>`,
+  OpeningTag: node => `<${node.component} ${node.props.map(prop => serialize(prop)).join(' ')}>`,
+  SelfClosingTag: node =>
+    `<${node.component} ${node.props.map(prop => serialize(prop)).join(' ')} />`,
+  Prop: node =>
+    `${node.name}=${
+      typeof node.value === 'string'
+        ? `"${node.value}"`
+        : `{${LiteralSerializer.serialize(node.value)}}`
+    }`,
+};
+
+function serialize(node: Node): string {
+  return SERIALIZER[node.type](node as any);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { Parser } from './Parser';
+export { Serializer } from './Serializer';
 export { Node, NodeIs, NodeType, Prop, Props, Tag, Text } from './Node';
